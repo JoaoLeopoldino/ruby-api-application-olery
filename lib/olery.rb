2bd4f0143ff74416f36ffb6adccac91e4f341b4e
João Leopoldino <leopogu@hotmail.com>
2022-10-09 14:27:17 +0100

    loading models

--- lib/olery.rb
+++ lib/olery.rb
@@ -13,6 +13,10 @@ require_relative 'olery/controllers/v1/users'
 #Models
 require_relative 'olery/models/user'
 require_relative 'olery/models/contract'
+require_relative 'olery/models/hotel'
+require_relative 'olery/models/hotel_group'
+require_relative 'olery/models/group_subscription'
+require_relative 'olery/models/group'
 
 #Server
 require_relative 'olery/v1_server'
